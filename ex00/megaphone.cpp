54389ac2e8bca34fbfe25d3134803662bad5acc6
Fabricio Lopez Reyes <flopez-r@c1r11s3.42madrid.com>
2024-05-02 17:31:07 +0200

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flopez-r <flopez-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 16:33:58 by flopez-r          #+#    #+#             */
+/*   Updated: 2024/05/02 17:17:44 by flopez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iostream"
+
+char	ft_upper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int main(int argc, char const *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+		return (0);
+	}
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			printf("%c", ft_upper(argv[i][j]));
+			j++;
+		}
+		i++;
+	}
+	printf ("%c", '\n');
+	return (0);
+}
+
