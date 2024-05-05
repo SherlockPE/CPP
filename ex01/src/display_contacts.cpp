1db74711f1fea2f5d0780e6cb47a1adfcaa18878
SherlockPE <fabriciolopezreyes@gmail.com>
2024-05-05 13:54:48 +0200

    display_contacts() function 1rst part done

--- ex01/src/display_contacts.cpp
+++ ex01/src/display_contacts.cpp
@@ -6,7 +6,7 @@
 /*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 18:47:13 by fabriciolop       #+#    #+#             */
-/*   Updated: 2024/05/05 00:07:09 by fabriciolop      ###   ########.fr       */
+/*   Updated: 2024/05/05 13:52:58 by fabriciolop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,46 @@ void	print_arr_val(std::string message, std::string value)
 	std::cout << message << value << std::endl;
 }
 
-void     put_field(std::string content)
+void	put_field(std::string content)
 {
-  int c_len;
-  int i;
- 
-  c_len = ft_strlen (content);
-  if (c_len > WSIZE)
-  {
-    content[WSIZE - 1] = '.';
-    content[WSIZE] = 0;
-  }
+	int	c_len;
+	int	i;
 
-  i = 0;
-  std::cout << "|";
-  std::cout << std::setfill (' ') << std::setw (WSIZE - c_len + 1);
-  while (i < WSIZE && content[i])
-  {
-    std::cout << content[i];
-    i++;
-  }
-  std::cout << "|";
+	c_len = ft_strlen(content);
+	if (c_len > WSIZE)
+	{
+		content[WSIZE - 1] = '.';
+		content[WSIZE] = 0;
+	}
+	i = 0;
+	//   std::cout << "|";
+	std::cout << std::setfill(' ') << std::setw(WSIZE - c_len + 1);
+	while (i < WSIZE && content[i])
+	{
+		std::cout << content[i];
+		i++;
+	}
+	std::cout << "|";
+	//   std::cout << "|";
 }
 
 void	print_titles(void)
 {
+	std::cout << "|";
 	put_field("Index");
 	put_field("First name");
 	put_field("Last name");
 	put_field("Nick name");
+	std::cout << std::endl;
+}
+
+void	put_rows(contact *phone_list, int position)
+{
+	std::cout << "|";
+	put_field("0");
+	put_field(phone_list[position].first_name);
+	put_field(phone_list[position].last_name);
+	put_field(phone_list[position].nick_name);
 	std::cout << std::endl;
 }
 
@@ -67,12 +78,7 @@ void	display_contacts(contact *phone_list, int *index)
 	print_titles();
 	while (i < *index)
 	{
-		// put_field(phone_list[i].id + '0');
-		put_field("r0");
-		put_field(phone_list[i].first_name);
-		put_field(phone_list[i].last_name);
-		put_field(phone_list[i].nick_name);
-		std::cout << std::endl;
+		put_rows(phone_list, i);
 		i++;
 	}
 }
@@ -80,7 +86,6 @@ void	display_contacts(contact *phone_list, int *index)
 // Desplegando lista de contactos
 // |Last name||    Index||First name||Nick name|
 // |       r0||      asa||        s||         |
-
 
 // |    Index||First name||Last name||Nick name|
 // |       r0||      asa||        s||        s|
