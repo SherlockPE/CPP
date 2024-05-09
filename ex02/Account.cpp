4e23a9ec026bb4a76b14e737d87c83d03a537a45
SherlockPE <fabriciolopezreyes@gmail.com>
2024-05-09 18:40:35 +0200

    Setters and gettes seted Displayers also done Constructors less

---
+++ ex02/Account.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 17:52:40 by fabriciolop       #+#    #+#             */
+/*   Updated: 2024/05/09 18:32:35 by fabriciolop      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iomanip>
+#include <iostream>
+
+//Getters (Accuount info)
+int		Account::checkAmount( void ) const{return (_amount);}
+//Getters (general info)
+int	Account::getNbAccounts( void ){return (_nbAccounts);}
+int	Account::getTotalAmount( void ){return (_totalAmount);}
+int	Account::getNbDeposits( void ){return (_totalNbDeposits);}
+int	Account::getNbWithdrawals( void ){return (_totalNbWithdrawals);}
+
+//Setters
+void	Account::makeDeposit( int deposit )
+{
+	_amount += deposit;
+	_nbDeposits += 1;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_amount -= withdrawal;
+	_nbWithdrawals += 1;
+}
+
+//Display Account info
+void	Account::displayStatus( void ) const
+{
+	std::cout << "index:" << _accountIndex;
+	std::cout << "amount:" << _amount;
+	std::cout << "deposits:" << _nbDeposits;
+	std::cout << "withdrawals:" << _nbWithdrawals;
+}
+//Display General Account's info
+void	Account::displayAccountsInfos( void )
+{
+	std::cout << "accounts:" << getNbAccounts();
+	std::cout << "total:" << getTotalAmount();
+	std::cout << "deposits:" << getNbDeposits();
+	std::cout << "withdrawals:" << getNbWithdrawals();
+}
+
+//Constructors
+Account::Account(int initial_deposit)
+{
+	_accountIndex = 0;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+}
+Account::~Account( void )
+{
+	
+}
+
+
+
