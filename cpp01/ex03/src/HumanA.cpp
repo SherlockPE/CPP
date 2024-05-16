b3f6ea228d727deccb3fab95e6f81e5437afa707
Fabricio Lopez Reyes <flopez-r@c1r13s2.42madrid.com>
2024-05-16 17:00:42 +0200

    added new lines to attack() function message

--- cpp01/ex03/src/HumanA.cpp
+++ cpp01/ex03/src/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 18:54:00 by fabriciolop       #+#    #+#             */
-/*   Updated: 2024/05/16 14:31:32 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:41:15 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	HumanA::attack(void)
 {
 	std::cout	<< _name
 				<< " attacks with their "
-				<< _weapon.getType();
+				<< _weapon.getType() << std::endl;
 }
 
 // Constructor
--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 14:07:33 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/05/16 14:31:45 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:33:12 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	HumanB::attack(void)
 {
 	std::cout	<< _name
 				<< " attacks with their "
-				<< _weapon.getType();
+				<< _weapon.getType() << std::endl;
 }
 
 void    HumanB::setWeapon(Weapon weapon)
