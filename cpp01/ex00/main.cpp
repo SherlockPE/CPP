e7bc9509eae220dac8d66bfe1d29da80087a2d73
Sherlock PE <flopez-r@student.42madrid.com>
2024-05-28 15:25:47 +0200

    pruebade commit

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 23:20:45 by fabriciolop       #+#    #+#             */
-/*   Updated: 2024/05/11 13:59:02 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:24:45 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,13 @@ void    d_leaks(void)
     system("leaks zombies00");
 }
 
-// int main (void)
-// {
-//     Zombie  doctor;
-
-//     atexit(d_leaks);
-//     doctor.randomChump("no Cerebros");
-//     // cubeta.newZombie("delfin");
-//     // cubeta.randomChump("Soy el de la cubeta");
-//     return (0);
-// }
-
 int main (void)
 {
     Zombie  *doctor;
 
-    // atexit(d_leaks);
     doctor = 0;
     doctor = newZombie("Cerebros");
     doctor->announce();
-    // cubeta.newZombie("delfin");
-    // cubeta.randomChump("Soy el de la cubeta");
     delete doctor;
     return (0);
 }
