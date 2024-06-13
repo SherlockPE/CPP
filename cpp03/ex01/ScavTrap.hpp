f93eb4048133fb6c4f5040494fefc332b91f1a06
Sherlock PE <flopez-r@student.42madrid.com>
2024-06-13 17:04:59 +0200

    starting with ex01

--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -1,35 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/13 16:21:13 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/06/13 16:56:02 by flopez-r         ###   ########.fr       */
+/*   Created: 2024/06/13 16:58:32 by flopez-r          #+#    #+#             */
+/*   Updated: 2024/06/13 17:01:50 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#pragma once
+#ifndef SCAV_TRAP_HPP
+# define SCAV_TRAP_HPP
 
-int main(void)
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
 {
-	ClapTrap	_default;
-	ClapTrap	_param("Biddle");
-	ClapTrap	_copy_constr(_param);
+    private:
+        /* data */
+    public:
+        ScavTrap(/* args */);
+        ~ScavTrap();
+};
 
-	int	i = 11;
-	while (i--)
-	{
-		_default.attack("alguien");
-		_param.attack("alguien");
-		_copy_constr.attack("alguien");
-	}
-	_copy_constr.beRepaired(2);
-
-	_copy_constr.takeDamage(500);
-	_copy_constr.attack("some");
-	_param.attack("some");
-	
-	return 0;
+ScavTrap::ScavTrap(/* args */)
+{
 }
+
+ScavTrap::~ScavTrap()
+{
+}
+
+
+
+
+#endif
