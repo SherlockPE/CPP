14c6ef74cad1e6f181fb1b331f5122f13b943830
Sherlock PE <flopez-r@student.42madrid.com>
2024-09-13 19:20:48 +0200

    Added Forwards declarations to avoid circulary dependences

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,83 +6,30 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 16:26:33 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/09/11 18:48:46 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/09/13 17:41:47 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main(int argc, char const *argv[])
 {
 	(void)argc;
 	(void)argv;
-
 	{
-		std::cout << YELLOW << "\nPrimera prueba\n" << NC ;
 		try
 		{
-			Bureaucrat not_name;
-			Bureaucrat Juan("Juan", 1);
+			Form peace("Tratado de paz", 3, 1);
 
-			std::cout << not_name;
-			std::cout << Juan;
-
-			Juan = not_name;
-
-			std::cout << not_name;
-			std::cout << Juan;
+			
+			std::cout << peace << std::endl;
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
-	}
-	{
-		std::cout << YELLOW << "\nSegunda prueba\n" << NC ;
-		try
-		{
-			Bureaucrat	Michael("Michael", 1);
-			Bureaucrat	copia_michael(Michael);
-
-			std::cout << Michael << std::endl;
-			std::cout << copia_michael << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	{
-		std::cout << YELLOW << "\nTercera prueba\n" << NC ;
-		try
-		{
-			Bureaucrat	Miguel("Miguel", 145);
-
-			std::cout << Miguel << std::endl;
-			for (int i = 150; i > 0; i--)
-				Miguel.subGrade();
-			std::cout << Miguel << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	{
-		std::cout << YELLOW << "\nCuarta prueba\n" << NC ;
-		try
-		{
-			Bureaucrat	Pablo("Pablo", 5);
-
-			std::cout << Pablo << std::endl;
-			for (int i = 0; i < 149; i++)
-				Pablo.addGrade();
-			std::cout << Pablo << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+		
 	}
 	return 0;
 }
