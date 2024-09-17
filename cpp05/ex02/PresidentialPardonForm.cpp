94d16b9821c539948ad425076a99876666fa45ed
Sherlock PE <flopez-r@student.42madrid.com>
2024-09-17 18:23:53 +0200

    Added Presidential pardon

---
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 18:15:41 by flopez-r          #+#    #+#             */
+/*   Updated: 2024/09/17 18:20:48 by flopez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+
+/*					Constructors					*/
+PresidentialPardonForm::PresidentialPardonForm(std::string name, int sign_grade, int exec_grade) :
+	AForm(name, sign_grade, exec_grade)
+{
+	std::cout << GREEN "PresidentialPardonForm default constructor called" NC << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const &other) :
+	AForm(other.get_form_name(), other.get_grade_to_sign(), other.get_grade_to_exec())
+{
+	std::cout << GREEN "PresidentialPardonForm copy constructor called" NC << std::endl;
+	*this = other;
+}
+
+/*					Operators					*/
+PresidentialPardonForm &PresidentialPardonForm::operator=(PresidentialPardonForm const &other)
+{
+	if (this == &other){
+		return (*this);
+	}
+	set_signed_value(other.get_signed_value());
+	return (*this);
+}
+
+/*					Destructor					*/
+PresidentialPardonForm::~PresidentialPardonForm(void) {
+	std::cout << RED "PresidentialPardonForm destructor called" NC << std::endl;
+}
+
+/*					Metods					*/
+void		PresidentialPardonForm::execute(Bureaucrat const &executor) const
+{
+	if (get_signed_value() == false)
+	{
+		std::cout << executor.getName() << MAGENTA << "Can't execute 'couse the form need to be signed" << NC << std::endl;
+		return ;
+	}
+	if (executor.getGrade() > get_grade_to_exec())
+	{
+		std::cout << executor.getName() << MAGENTA << "Can't execute the Form becouse it doesnt have enought grade" << NC << std::endl;
+		throw (GradeTooLowException());
+	}
+	std::cout << executor.getName() << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,64 +6,89 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 16:26:33 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/09/17 18:13:04 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:23:05 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main(void)
 {
+	// {
+	// 	try
+	// 	{		
+	// 		Bureaucrat				General("General", 1);
+	// 		Bureaucrat				Novato("Novato", 150);
+	// 		ShrubberyCreationForm	TreeForm("Arbustos", 50, 1);
+
+	// 		// Print group
+	// 		std::cout << General << Novato << TreeForm << std::endl;
+
+	// 		// Sign formulary
+	// 		// Novato.signForm(TreeForm);
+	// 		General.signForm(TreeForm);
+	// 		std::cout << TreeForm;
+
+	// 		// Execute Formulary
+	// 		// TreeForm.execute(Novato);
+	// 		TreeForm.execute(General);
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}		
+	// 	return (0);
+	// }
+	// {
+	// 	std::srand(time(0));
+	// 	try
+	// 	{
+	// 		Bureaucrat				General("General", 1);
+	// 		Bureaucrat				Novato("Novato", 150);
+	// 		RobotomyRequestForm		Robot("Robot", 50, 1);
+
+	// 		// Print group
+	// 		std::cout << General << Novato << Robot << std::endl;
+
+	// 		// Sign formulary
+	// 		// Novato.signForm(Robot);
+	// 		General.signForm(Robot);
+	// 		std::cout << Robot;
+
+	// 		// Execute Formulary
+	// 		// Robot.execute(Novato);
+	// 		Robot.execute(General);
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}
+	// }
 	{
-		std::srand(time(0));
 		try
 		{
 			Bureaucrat				General("General", 1);
 			Bureaucrat				Novato("Novato", 150);
-			RobotomyRequestForm		Robot("Robot", 50, 1);
+			PresidentialPardonForm	PardonForm("PardonForm", 50, 1);
 
 			// Print group
-			std::cout << General << Novato << Robot << std::endl;
+			std::cout << General << Novato << PardonForm << std::endl;
 
 			// Sign formulary
-			// Novato.signForm(Robot);
-			General.signForm(Robot);
-			std::cout << Robot;
+			// Novato.signForm(PardonForm);
+			General.signForm(PardonForm);
+			std::cout << PardonForm;
 
 			// Execute Formulary
-			// Robot.execute(Novato);
-			Robot.execute(General);
+			// PardonForm.execute(Novato);
+			PardonForm.execute(General);
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
-	}
-	{
-		try
-		{		
-			Bureaucrat				General("General", 1);
-			Bureaucrat				Novato("Novato", 150);
-			ShrubberyCreationForm	TreeForm("Arbustos", 50, 1);
-
-			// Print group
-			std::cout << General << Novato << TreeForm << std::endl;
-
-			// Sign formulary
-			// Novato.signForm(TreeForm);
-			General.signForm(TreeForm);
-			std::cout << TreeForm;
-
-			// Execute Formulary
-			// TreeForm.execute(Novato);
-			TreeForm.execute(General);
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}		
-		return (0);
 	}
 }
