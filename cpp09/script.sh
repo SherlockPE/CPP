4ea5a4123122e8f3e5c4f48b4c9daa901ecc78ab
Sherlock PE <flopez-r@student.42madrid.com>
2024-11-08 08:45:20 +0100

    Ready to start with cpp09

--- cpp09/script.sh
+++ cpp09/script.sh
@@ -6,6 +6,6 @@ while [ $i -le 2 ]
 do
     mkdir -p "ex0$i"
     touch "ex0$i/main.cpp"
-    cat ../cpp06/ex02/Makefile > "ex0$i/Makefile"
+    cat ../cpp08/ex02/Makefile > "ex0$i/Makefile"
     i=$(($i + 1))
 done
